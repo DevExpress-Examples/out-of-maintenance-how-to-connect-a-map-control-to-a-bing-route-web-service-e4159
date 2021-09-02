360aaa7a641784ebbc371e43eb1ede2bb2f93028
DevExpressExampleBot <fakeEmail>
2021-09-02 18:44:53 +0300

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -6,34 +6,36 @@ Imports System.Windows.Input
 
 Namespace DXMap_RouteProvider
 
-    Partial Public Class MainWindow
-        Inherits Window
+	Partial Public Class MainWindow
+		Inherits Window
 
-        #Region "#CalculateRoute"
-        Public Sub New()
-            InitializeComponent()
+		#Region "#CalculateRoute"
+		Public Sub New()
+			InitializeComponent()
 
-            ' Create three waypoints and add them to a route waypoints list. 
-            Dim waypoints As New List(Of RouteWaypoint)()
-            waypoints.Add(New RouteWaypoint("New York", New GeoPoint(41.145556, -73.995)))
-            waypoints.Add(New RouteWaypoint("Oklahoma", New GeoPoint(36.131389, -95.937222)))
-            waypoints.Add(New RouteWaypoint("Las Vegas", New GeoPoint(36.175, -115.136389)))
+			' Create three waypoints and add them to a route waypoints list. 
+			Dim waypoints As New List(Of RouteWaypoint)()
+			waypoints.Add(New RouteWaypoint("New York", New GeoPoint(41.145556, -73.995)))
+			waypoints.Add(New RouteWaypoint("Oklahoma", New GeoPoint(36.131389, -95.937222)))
+			waypoints.Add(New RouteWaypoint("Las Vegas", New GeoPoint(36.175, -115.136389)))
 
-            routeProvider.CalculateRoute(waypoints)
-        End Sub
+			routeProvider.CalculateRoute(waypoints)
+		End Sub
 
-        Private Sub routeProvider_LayerItemsGenerating(ByVal sender As Object, ByVal args As LayerItemsGeneratingEventArgs)
-            Dim letter As Char = "A"c
+		Private Sub routeProvider_LayerItemsGenerating(ByVal sender As Object, ByVal args As LayerItemsGeneratingEventArgs)
+			Dim letter As Char = "A"c
 
-            For Each item As MapItem In args.Items
-                Dim pushpin As MapPushpin = TryCast(item, MapPushpin)
-                If pushpin IsNot Nothing Then
-                    pushpin.Text = letter.ToString()
-                    letter = ChrW(AscW(letter) + 1)
-                End If
-            Next item
-        End Sub
+			For Each item As MapItem In args.Items
+				Dim pushpin As MapPushpin = TryCast(item, MapPushpin)
+				If pushpin IsNot Nothing Then
+'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
+'ORIGINAL LINE: pushpin.Text = letter++.ToString();
+					pushpin.Text = letter.ToString()
+					letter = ChrW(AscW(letter) + 1)
+				End If
+			Next item
+		End Sub
 
-        #End Region ' #CalculateRoute
-    End Class
+		#End Region ' #CalculateRoute
+	End Class
 End Namespace
