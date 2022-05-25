65a449774081a86684e2895cdd64c0524e98f5e0
DevExpressExampleBot <fakeEmail>
2022-05-25 16:23:27 +0400

    autogenerate VB

---
+++ VB/App.xaml.vb
@@ -0,0 +1,12 @@
+Imports System.Windows
+
+Namespace DXMap_RouteProvider
+
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,41 +1,36 @@
-﻿Imports DevExpress.Xpf.Map
+Imports DevExpress.Xpf.Map
 Imports System.Collections.Generic
-Imports System.Collections.ObjectModel
 Imports System.Windows
-Imports System.Windows.Input
+Imports System.Windows.Media
 
 Namespace DXMap_RouteProvider
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		#Region "#CalculateRoute"
-		Public Sub New()
-			InitializeComponent()
+        Public Sub New()
+            Me.InitializeComponent()
+            ' Create three waypoints and add them to a route waypoints list. 
+            Dim waypoints As List(Of RouteWaypoint) = New List(Of RouteWaypoint)()
+            waypoints.Add(New RouteWaypoint("New York", New GeoPoint(41.145556, -73.995)))
+            waypoints.Add(New RouteWaypoint("Oklahoma", New GeoPoint(36.131389, -95.937222)))
+            waypoints.Add(New RouteWaypoint("Las Vegas", New GeoPoint(36.175, -115.136389)))
+            Me.routeProvider.CalculateRoute(waypoints)
+        End Sub
 
-			' Create three waypoints and add them to a route waypoints list. 
-			Dim waypoints As New List(Of RouteWaypoint)()
-			waypoints.Add(New RouteWaypoint("New York", New GeoPoint(41.145556, -73.995)))
-			waypoints.Add(New RouteWaypoint("Oklahoma", New GeoPoint(36.131389, -95.937222)))
-			waypoints.Add(New RouteWaypoint("Las Vegas", New GeoPoint(36.175, -115.136389)))
+        Private Sub routeProvider_LayerItemsGenerating(ByVal sender As Object, ByVal args As LayerItemsGeneratingEventArgs)
+            Dim letter As Char = "A"c
+            For Each item As MapItem In args.Items
+                Dim pushpin As MapPushpin = TryCast(item, MapPushpin)
+                If pushpin IsNot Nothing Then pushpin.Text = System.Math.Min(System.Threading.Interlocked.Increment(letter), letter - 1).ToString()
+                Dim line As MapPolyline = TryCast(item, MapPolyline)
+                If line IsNot Nothing Then
+                    line.Fill = Brushes.Red
+                    line.Stroke = Brushes.Red
+                End If
+            Next
 
-			routeProvider.CalculateRoute(waypoints)
-		End Sub
-
-		Private Sub routeProvider_LayerItemsGenerating(ByVal sender As Object, ByVal args As LayerItemsGeneratingEventArgs)
-			Dim letter As Char = "A"c
-
-			For Each item As MapItem In args.Items
-				Dim pushpin As MapPushpin = TryCast(item, MapPushpin)
-				If pushpin IsNot Nothing Then
-'INSTANT VB WARNING: An assignment within expression was extracted from the following statement:
-'ORIGINAL LINE: pushpin.Text = letter++.ToString();
-					pushpin.Text = letter.ToString()
-					letter = ChrW(AscW(letter) + 1)
-				End If
-			Next item
-		End Sub
-
-		#End Region ' #CalculateRoute
-	End Class
+            Me.map.ZoomToFit(args.Items)
+        End Sub
+    End Class
 End Namespace
